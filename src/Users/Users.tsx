a119a01eb8f2b5ede27408a6cbb5b4754c08ecad
suraj_redxcel <suraj@redxcel.com>
2022-11-18 18:37:00 +0530

    added toolbar and styles to datagrid

--- src/Users/Users.tsx
+++ src/Users/Users.tsx
@@ -18,10 +18,18 @@ import AddIcon from '@mui/icons-material/Add'
 import Fab from '@mui/material/Fab';
 
 
-import { DataGrid, GridColDef } from '@mui/x-data-grid'
+import { 
+    DataGrid,
+    GridColDef,
+    GridToolbarContainer,
+    GridToolbarColumnsButton,
+    GridToolbarFilterButton,
+    GridToolbarExport,
+    GridToolbarDensitySelector, } from '@mui/x-data-grid'
 import { Box } from '@mui/material'
-import { gridClasses } from '@mui/system'
+import { gridClasses } from '@mui/material'
 
+import { alpha, styled } from '@mui/material/styles'
 // import { AiOutlineDrag } from 'react-icons/ai'
 // import { MdOutlineDragHandle } from 'react-icons/md'
 // import Table from '@mui/material/Table'
@@ -31,6 +39,17 @@ import { gridClasses } from '@mui/system'
 // import TableHead from '@mui/material/TableHead'
 // import TableRow from '@mui/material/TableRow'
 // import { Button } from '@mui/material'
+
+function CustomToolbar() {
+    return (
+      <GridToolbarContainer sx={{padding: '0.5rem 1rem', gap: '1rem', ["& .MuiButton-root"]: { padding: '0.4rem 0.8rem' }}}>
+        <GridToolbarColumnsButton />
+        <GridToolbarFilterButton />
+        <GridToolbarDensitySelector />
+        <GridToolbarExport />
+      </GridToolbarContainer>
+    );
+  }
 
 function Users() {
     const [users, setUsers] = useState<employeesType[]>([])
@@ -97,6 +116,41 @@ function Users() {
         getUsers()
     }, [])
 
+    const ODD_OPACITY = 0.2;
+
+const StripedDataGrid = styled(DataGrid)(({ theme }) => ({
+  ["& .MuiDataGrid-row:nth-of-type(even)"]: {
+    backgroundColor: theme.palette.grey[200],
+    '&:hover, &.Mui-hovered': {
+      backgroundColor: alpha(theme.palette.primary.main, ODD_OPACITY),
+      '@media (hover: none)': {
+        backgroundColor: 'transparent',
+      },
+    },
+    '&.Mui-selected': {
+      backgroundColor: alpha(
+        theme.palette.primary.main,
+        ODD_OPACITY + theme.palette.action.selectedOpacity,
+      ),
+      '&:hover, &.Mui-hovered': {
+        backgroundColor: alpha(
+          theme.palette.primary.main,
+          ODD_OPACITY +
+            theme.palette.action.selectedOpacity +
+            theme.palette.action.hoverOpacity,
+        ),
+        // Reset on touch devices, it doesn't add specificity
+        '@media (hover: none)': {
+          backgroundColor: alpha(
+            theme.palette.primary.main,
+            ODD_OPACITY + theme.palette.action.selectedOpacity,
+          ),
+        },
+      },
+    },
+  },
+}));
+
     const colDef: GridColDef[] = useMemo(()=> [
         {
             field: 'id', headerName: 'No.', maxWidth: 40, renderCell: (params) => {
@@ -147,7 +201,7 @@ function Users() {
                     </Tooltip>
                 </span>
             </div>
-            <DataGrid
+            <StripedDataGrid
                 rows={users}
                 columns={colDef}
                 autoHeight
@@ -158,6 +212,9 @@ function Users() {
                     top: params.isFirstVisible ? 0 : 5,
                     bottom: params.isLastVisible ? 0 : 5
                 })}
+                components={{
+                    Toolbar: CustomToolbar
+                }}
             />
 
             <dialog id='add-user-modal' ref={addUserModal}>
