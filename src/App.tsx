0b601c0be2442a9c94ed8a0b860ec3cd58396976
suraj_redxcel <suraj@redxcel.com>
2022-11-18 18:38:22 +0530

    -

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React, { useEffect, useRef, useState } from 'react'
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import Users from './Users/Users';
-import User from './Users/User';
-import NotFound from './NotFound/NotFound';
-import PageBuilder from './PageBuilder/PageBuilder';
-import UBoard from './UBoard/UBoard';
-import Invite from './Invite/Invite';
+import React, { useState } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+import Users from './Users/Users'
+import User from './Users/User'
+import NotFound from './NotFound/NotFound'
+import PageBuilder from './PageBuilder/PageBuilder'
+import UBoard from './UBoard/UBoard'
+import Invite from './Invite/Invite'
 import Navbar from './Navbar/Navbar'
-// import FormBuilder from './FormBuilder/FormBuilder';
-import CvBuilder from './CvBuilder/CvBuilder';
-import SchemaBuilder from './SchemaBuilder/SchemaBuilder';
-import Search from './Search/Search';
-import Offers from './Offers/Offers';
+import CvBuilder from './CvBuilder/CvBuilder'
+import SchemaBuilder from './SchemaBuilder/SchemaBuilder'
+import Offers from './Offers/Offers'
 import Mix from './Mix/Mix'
-import Dash from './Dash/Dash';
-import Sidenav from './Sidenav/Sidenav';
-import Faq from './Faq/Faq';
+import Dash from './Dash/Dash'
+import Sidenav from './Sidenav/Sidenav'
+import Faq from './Faq/Faq'
 import { FcNext } from 'react-icons/fc'
+import Quotation from './Quotation/Quotation'
 
 function App() {
-  const [isSideNav, setIsSideNav] = useState(false);
+  const [isSideNav, setIsSideNav] = useState(false)
 
   return (
     <div>
@@ -46,11 +45,11 @@ function App() {
               {/* <Route path='/forms' element={<FormIo />} /> */}
               <Route path='/cvbuilder' element={<CvBuilder />} />
               <Route path='/formbuilder' element={<SchemaBuilder />} />
-              <Route path='/search' element={<Search />} />
               <Route path='/offers' element={<Offers />} />
               <Route path='/mix' element={<Mix />} />
               <Route path='/uboard' element={<UBoard />} />
               <Route path='/faq' element={<Faq />} />
+              <Route path='quotation' element={<Quotation />} />
             </Routes>
           </div>
         </div>
